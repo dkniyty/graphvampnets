eb431e0357205dbd5909d2de2d524d93e9068afc
dkniyty <123341552+dkniyty@users.noreply.github.com>
2024-07-16 23:19:11 -0400

    Update dataprocessing.py

--- graphvampnets/processing/dataprocessing.py
+++ graphvampnets/processing/dataprocessing.py
@@ -8,7 +8,7 @@ base_dir = os.path.abspath(os.path.join(current_dir, '..', '..'))
 if base_dir not in sys.path:
     sys.path.append(base_dir)
 
-from graphvampnets.utils import rao_blackwell_ledoit_wolf
+from utils import rao_blackwell_ledoit_wolf
 
 class Preprocessing:
     """ Preprocess the original trajectories to create datasets for training.
